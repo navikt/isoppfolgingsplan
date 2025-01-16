b367a9e7a0f0e99c52956cb1ab1580c996ced2dc
Vetle Bu Solgård <vetle.bu.solgard@nav.no>
2025-01-16 09:12:04 +0100

    IS-2999: Remove veilederident from payload (#20)

--- src/main/kotlin/no/nav/syfo/api/model/ForesporselRequestDTO.kt
+++ src/main/kotlin/no/nav/syfo/api/model/ForesporselRequestDTO.kt
@@ -2,7 +2,6 @@ package no.nav.syfo.api.model
 
 data class ForesporselRequestDTO(
     val arbeidstakerPersonident: String,
-    val veilederident: String,
     val virksomhetsnummer: String,
     val narmestelederPersonident: String,
 )
