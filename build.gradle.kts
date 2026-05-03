196cfc3ea131671432ba6f49af8fcc4575b438fe
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2026-05-03 20:33:05 +0200

    Upgrade to isyfo-backend-utils 0.0.5, rename getPersonIdent to getPersonident

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
-    implementation("no.nav.syfo:isyfo-backend-utils:0.0.4")
+    implementation("no.nav.syfo:isyfo-backend-utils:0.0.5")
 
     implementation("io.ktor:ktor-client-apache:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
