5f287f841fdc5ad6429e302d6da6788717fd6ee9
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2026-05-16 14:56:37 +0200

    Bump isyfo-backend-common to 0.0.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 
-    implementation("no.nav.syfo:isyfo-backend-common:0.0.10")
+    implementation("no.nav.syfo:isyfo-backend-common:0.0.11")
 
     implementation("io.ktor:ktor-client-apache:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
