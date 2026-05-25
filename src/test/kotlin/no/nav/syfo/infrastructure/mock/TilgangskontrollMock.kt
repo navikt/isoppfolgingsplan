03943bed2a809a3dad6c217a2dc4a4c75051ff1f
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2026-05-25 18:05:01 +0200

    Get tilgangskontroll client paths from library and bump isyfo-backend-common to 0.0.24

--- src/test/kotlin/no/nav/syfo/infrastructure/mock/TilgangskontrollMock.kt
+++ src/test/kotlin/no/nav/syfo/infrastructure/mock/TilgangskontrollMock.kt
@@ -4,10 +4,9 @@ import io.ktor.client.engine.mock.*
 import io.ktor.client.request.*
 import kotlinx.coroutines.runBlocking
 import no.nav.syfo.UserConstants.ARBEIDSTAKER_PERSONIDENT_VEILEDER_NO_ACCESS
+import no.nav.syfo.common.tilgangskontroll.client.TilgangskontrollClient.Companion.TILGANGSKONTROLL_BRUKERE_PATH
+import no.nav.syfo.common.tilgangskontroll.client.TilgangskontrollClient.Companion.TILGANGSKONTROLL_PERSON_PATH
 import no.nav.syfo.infrastructure.NAV_PERSONIDENT_HEADER
-
-private const val TILGANGSKONTROLL_PERSON_PATH = "/api/tilgang/navident/person"
-private const val TILGANGSKONTROLL_BRUKERE_PATH = "/api/tilgang/navident/brukere"
 
 private data class TilgangResponse(
     val erGodkjent: Boolean,
